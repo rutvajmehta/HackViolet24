32179f06a450f466120c3dee4f10416d58675503
Jayantd11 <130610628+Jayantd11@users.noreply.github.com>
2024-02-04 08:34:57 -0500

    Update callCode.py

--- callCode.py
+++ callCode.py
@@ -5,14 +5,14 @@ from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 
-account_sid = "AC7d292b17e3788c71c5b8004c8632fc0f"
-auth_token = "f52b53f0218be47856ea6d7bce762325"
+account_sid = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+auth_token = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
   url="http://demo.twilio.com/docs/voice.xml",
-  to="+18582509640",
-  from_="+18332477947"
+  to="+1123456789",
+  from_="+1123456789"
 )
 
 print(call.sid)
